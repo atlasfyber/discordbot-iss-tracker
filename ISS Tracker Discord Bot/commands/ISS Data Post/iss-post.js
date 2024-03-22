3ce2f6ff9e27228120b2b9ec6065ee392d4200d9
AstroFyber <78761344+astrofyber@users.noreply.github.com>
2024-03-22 09:43:53 +0200

    Update iss-post.js

--- ISS Tracker Discord Bot/commands/ISS Data Post/iss-post.js
+++ ISS Tracker Discord Bot/commands/ISS Data Post/iss-post.js
@@ -61,7 +61,7 @@ $elseif[$getVar[iss_country;automations]!=??&&$getVar[iss_country;automations]!=
 {button:Map URL:link:https#COLON#//$getVar[iss_url;automations]:false}
 }]
 
-$onlyIf[$getGuildVar[iss_filter;$guildID;guilds_automations]==$getVar[iss_country;automations];] 
+$onlyIf[$geVar[iss_filter;automations]==$getVar[iss_country;automations];] 
 
 $suppressErrors[]
 
