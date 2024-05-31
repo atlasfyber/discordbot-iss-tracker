c30b54e77db6a620b1feb5b6a55db2e1544ca4a4
Atlas Fyber <78761344+atlasfyber@users.noreply.github.com>
2024-05-31 12:06:39 +0300

    Const Name Change | Github Log Name Change | Minor Fixes

--- ISS Tracker Discord Bot/index.js
+++ ISS Tracker Discord Bot/index.js
@@ -4,7 +4,7 @@
 const { AoiClient, LoadCommands, Util } = require("aoi.js");
 
 // Create multiple instances of Aoi.js
-const astrofyber = new AoiClient({
+const application = new AoiClient({
     token: "DISCORD BOT TOKEN HERE",
     prefix: "--",
     intents: ["MessageContent", "Guilds", "GuildMessages","GuildMembers"],
@@ -15,12 +15,12 @@ const astrofyber = new AoiClient({
         db: require("@akarui/aoi.db"),
         dbType: "KeyValue",
         tables: ["main","automations"],
-    securityKey: "ab3sP8dR6qX2vA7wL3mF9cB2hY4zabcd",
+    securityKey: "a-32-characters-long-string-here"
   }
 });
-$log[github/astrofyber | Commands are now live!]
+$log[github/atlasfyber | Commands are now live!]
 // Load commands for each instance
-astrofyber.loadCommands("./commands/", true);
+application.loadCommands("./commands/", true);
 
 // END OF MAIN FILE
 // -------------------------------------------------------------
@@ -28,11 +28,11 @@ astrofyber.loadCommands("./commands/", true);
 
 // The channel that you want to receive notifications
 // Make sure bot has perms to send messages to this channel
-astrofyber.variables({
+application.variables({
 channel: "CHANNEL ID HERE"}, "main")
 
 // It will send a message when the bot  finds and deliver new data
-astrofyber.variables({
+application.variables({
 logs_channel: "CHANNEL ID HERE"}, "main")
 
 
@@ -40,7 +40,7 @@ logs_channel: "CHANNEL ID HERE"}, "main")
 // It supports ONLY ISO 3166-1 alpha-2 codes
 // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 // Example: "US"
-astrofyber.variables({
+application.variables({
 iss_filter: "All Locations"}, "main")
 
 
@@ -49,30 +49,37 @@ iss_filter: "All Locations"}, "main")
 
 
 // PRE-CONFIGURED | DO NOT TOUCH IF YOU DONT KNOW HOW IT WORKS.
-astrofyber.variables({
+application.variables({
 iss_country: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_time: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_fullcountry: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_visibility: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_url: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_flag: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_longitude: "0"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_img: "https://cdn.discordapp.com/attachments/1154857744143224922/1220638298746392576/image.png?ex=660fab00&is=65fd3600&hm=598d0e3d9cae21971cd6aad587df87a054117ce6febc25d2d0d3c28fee78511a&"}, "automations")
-astrofyber.variables({
+application.variables({
 iss_latitude: "0"}, "automations")
-astrofyber.variables({
-footer: "ISS Tracker | github/astrofyber"}, "main")
+application.variables({
+footer: "ISS Tracker | github/application"}, "main")
 
 // DISCORD BOT STATUS
-astrofyber.status({
-  name: "ISS Tracker | github/astrofyber",
+application.status({
+  name: "ISS Tracker | github/atlasfyber",
+  type: "CUSTOM",
+  status: "online",
+  time: 40,
+});
+
+application.status({
+  name: "✨ Looking the stars, and searching..",
   type: "CUSTOM",
   status: "online",
   time: 40,
